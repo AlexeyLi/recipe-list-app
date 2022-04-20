bbae78bcbb4882c67e40fd2f16e1bd439e416194
Alexey Li <alexeyli.sdet@gmail.com>
2022-04-19 19:30:30 -0700

    Initial Commit

---
+++ Recipe List App/Recipe_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_List_AppApp.swift
+//  Recipe List App
+//
+//  Created by Alexey Li on 4/19/22.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
