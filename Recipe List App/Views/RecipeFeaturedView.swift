3b730028cafbd19d60f9bd2dc56a933ed6960138
Alexey Li <alexeyli.sdet@gmail.com>
2023-01-10 18:14:25 -0800

    Add button for recipe cards in featured view

--- Recipe List App/Views/RecipeFeaturedView.swift
+++ Recipe List App/Views/RecipeFeaturedView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct RecipeFeaturedView: View {
     
     @EnvironmentObject var model:RecipeModel
+    @State var isDetailViewShowing = false
     
     var body: some View {
         
@@ -28,24 +29,37 @@ struct RecipeFeaturedView: View {
                     // Loop through each recipe
                     ForEach(0..<model.recipes.count) { index in
                         
+                        var recipe = model.recipes[index]
+                        
                         // Only show those that should be featured
-                        if (model.recipes[index].featured) {
+                        if (recipe.featured) {
                             
-                            // Recipe card
-                            
-                            ZStack {
-                                Rectangle()
-                                    .foregroundColor(.white)
+                            Button(action: {
                                 
-                                VStack {
-                                    Image(model.recipes[index].image)
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fill)
-                                        .clipped()
-                                    Text(model.recipes[index].name)
-                                        .padding(5)
+                                // Show the recipe detail sheet
+                                self.isDetailViewShowing = true
+                                
+                            }, label: {
+                                // Recipe card
+                                ZStack {
+                                    Rectangle()
+                                        .foregroundColor(.white)
+                                    
+                                    VStack {
+                                        Image(recipe.image)
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fill)
+                                            .clipped()
+                                        Text(recipe.name)
+                                            .padding(5)
+                                    }
                                 }
+                            })
+                            .sheet(isPresented: $isDetailViewShowing) {
+                                // Show the Recipe Detail View
+                                RecipeDetailView(recipe: recipe)
                             }
+                            .buttonStyle(PlainButtonStyle())
                             .frame(width: geo.size.width - 40, height: geo.size.height - 100, alignment: .center)
                                 .cornerRadius(15)
                                 .shadow(color: Color(.sRGB, red: 0, green: 0, blue: 0, opacity: 0.5), radius: 10, x: -5, y: 5)
